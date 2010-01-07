95c812837143c067b49e7b7f27dd342cd1f444c0
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-01-06 23:34:07 -0600

    Added web service.

---
+++ UpdateControls.Correspondence.WebService/SynchronizationService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UpdateControls.Correspondence.WebService.Contract;
+
+namespace UpdateControls.Correspondence.WebService
+{
+    public class SynchronizationService : ISynchronizationService
+    {
+        public FactTree Get(FactTree rootTree, long rootId, long timestamp)
+        {
+            return new FactTree();
+        }
+
+        public void Post(FactTree messageBody)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
