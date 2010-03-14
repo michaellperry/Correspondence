d75a411f0d03b422c4960757c89c3a22a817e0ab
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-03-14 13:01:28 -0500

    Added T4 template.

---
+++ Reversi.GameModel.ExternalDSL/GameModel.generated.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UpdateControls.Correspondence;
+using UpdateControls.Correspondence.Mementos;
+using System;
+
+namespace Reversi.GameModel
+{
+    [CorrespondenceType]
+    public class Queue : CorrespondenceFact
+    {
+
+
+        [CorrespondenceField]
+        private string _identifier;
+
+        public Queue(
+            string identifier
+            )
+        {
+            _identifier = identifier;
+        }
+
+        public Queue(FactMemento memento)
+        {
+        }
+    }
+    [CorrespondenceType]
+    public class Time : CorrespondenceFact
+    {
+
+
+        [CorrespondenceField]
+        private DateTime _start;
+
+        public Time(
+            DateTime start
+            )
+        {
+            _start = start;
+        }
+
+        public Time(FactMemento memento)
+        {
+        }
+    }
+    [CorrespondenceType]
+    public class Frame : CorrespondenceFact
+    {
+        public static Role<Queue> Role_queue = new Role<Queue>("queue");
+        public static Role<Time> Role_timestamp = new Role<Time>("timestamp");
+
+        private PredecessorObj<Queue> _queue;
+        private PredecessorObj<Time> _timestamp;
+
+
+        public Frame(
+            Queue queue
+            ,Time timestamp
+            )
+        {
+            _queue = new PredecessorObj<Queue>(this, Role_queue, queue);
+            _timestamp = new PredecessorObj<Time>(this, Role_timestamp, timestamp);
+        }
+
+        public Frame(FactMemento memento)
+        {
+            _queue = new PredecessorObj<Queue>(this, Role_queue, memento);
+            _timestamp = new PredecessorObj<Time>(this, Role_timestamp, memento);
+        }
+    }
+}
