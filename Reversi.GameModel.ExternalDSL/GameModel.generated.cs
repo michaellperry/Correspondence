bb9496cbb514f678059d594c99f0be74519758a7
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-03-16 06:49:26 -0500

    Cosmetic changes to template

--- Reversi.GameModel.ExternalDSL/GameModel.generated.cs
+++ Reversi.GameModel.ExternalDSL/GameModel.generated.cs
@@ -9,11 +9,15 @@ namespace Reversi.GameModel
     [CorrespondenceType]
     public class Queue : CorrespondenceFact
     {
+        // Roles
 
+        // Predecessors
 
+        // Fields
         [CorrespondenceField]
         private string _identifier;
 
+        // Business constructor
         public Queue(
             string identifier
             )
@@ -21,12 +25,15 @@ namespace Reversi.GameModel
             _identifier = identifier;
         }
 
+        // Hydration constructor
         public Queue(FactMemento memento)
         {
         }
 
+        // Predecessor access
 
-        public string identifier
+        // Field access
+        public string Identifier
         {
             get { return _identifier; }
         }
@@ -35,11 +42,15 @@ namespace Reversi.GameModel
     [CorrespondenceType]
     public class Time : CorrespondenceFact
     {
+        // Roles
 
+        // Predecessors
 
+        // Fields
         [CorrespondenceField]
         private DateTime _start;
 
+        // Business constructor
         public Time(
             DateTime start
             )
@@ -47,12 +58,15 @@ namespace Reversi.GameModel
             _start = start;
         }
 
+        // Hydration constructor
         public Time(FactMemento memento)
         {
         }
 
+        // Predecessor access
 
-        public DateTime start
+        // Field access
+        public DateTime Start
         {
             get { return _start; }
         }
@@ -61,82 +75,103 @@ namespace Reversi.GameModel
     [CorrespondenceType]
     public class Frame : CorrespondenceFact
     {
-        public static Role<Queue> Role_queue = new Role<Queue>("queue");
-        public static Role<Time> Role_timestamp = new Role<Time>("timestamp");
+        // Roles
+        public static Role<Queue> RoleQueue = new Role<Queue>("queue");
+        public static Role<Time> RoleTimestamp = new Role<Time>("timestamp");
 
+        // Predecessors
         private PredecessorObj<Queue> _queue;
         private PredecessorObj<Time> _timestamp;
 
+        // Fields
 
+        // Business constructor
         public Frame(
             Queue queue
             ,Time timestamp
             )
         {
-            _queue = new PredecessorObj<Queue>(this, Role_queue, queue);
-            _timestamp = new PredecessorObj<Time>(this, Role_timestamp, timestamp);
+            _queue = new PredecessorObj<Queue>(this, RoleQueue, queue);
+            _timestamp = new PredecessorObj<Time>(this, RoleTimestamp, timestamp);
         }
 
+        // Hydration constructor
         public Frame(FactMemento memento)
         {
-            _queue = new PredecessorObj<Queue>(this, Role_queue, memento);
-            _timestamp = new PredecessorObj<Time>(this, Role_timestamp, memento);
+            _queue = new PredecessorObj<Queue>(this, RoleQueue, memento);
+            _timestamp = new PredecessorObj<Time>(this, RoleTimestamp, memento);
         }
 
-        public Queue queue
+        // Predecessor access
+        public Queue Queue
         {
             get { return _queue.Fact; }
         }
-        public Time timestamp
+        public Time Timestamp
         {
             get { return _timestamp.Fact; }
         }
 
+        // Field access
     }
     
     [CorrespondenceType]
     public class Player : CorrespondenceFact
     {
+        // Roles
 
+        // Predecessors
 
+        // Fields
 
+        // Business constructor
         public Player(
             )
         {
         }
 
+        // Hydration constructor
         public Player(FactMemento memento)
         {
         }
 
+        // Predecessor access
 
+        // Field access
     }
     
     [CorrespondenceType]
     public class Game : CorrespondenceFact
     {
-        public static Role<Player> Role_players = new Role<Player>("players");
+        // Roles
+        public static Role<Player> RolePlayers = new Role<Player>("players");
 
+        // Predecessors
         private PredecessorList<Player> _players;
 
+        // Fields
 
+        // Business constructor
         public Game(
             IEnumerable<Player> players
             )
         {
-            _players = new PredecessorList<Player>(this, Role_players, players);
+            _players = new PredecessorList<Player>(this, RolePlayers, players);
         }
 
+        // Hydration constructor
         public Game(FactMemento memento)
         {
-            _players = new PredecessorList<Player>(this, Role_players, memento);
+            _players = new PredecessorList<Player>(this, RolePlayers, memento);
         }
 
-        public IEnumerable<Player> players
+        // Predecessor access
+        public IEnumerable<Player> Players
         {
             get { return _players; }
         }
      
+        // Field access
     }
     
 }
