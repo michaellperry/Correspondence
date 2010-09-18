ae25d5eccfadc5b5ccf2e72ceb7e431d2c3ced9b
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-09-18 09:49:14 -0500

    Added Windows Phone 7 assemblies.

---
+++ WP7/UpdateControls.Correspondence (WP7)/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls.Correspondence")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("203af62e-6a4c-4f1a-9724-70539366f6e1")]
