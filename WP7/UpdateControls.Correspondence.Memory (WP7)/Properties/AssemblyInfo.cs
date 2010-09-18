5febba152796634e5623e2b0814b3cae5171421f
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-09-18 10:06:44 -0500

    Added Memory for WP7. Shell of WebServiceClient.

---
+++ WP7/UpdateControls.Correspondence.Memory (WP7)/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls.Correspondence.Memory")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("4F45D7BF-DDC2-468A-8371-02182F0AC609")]
