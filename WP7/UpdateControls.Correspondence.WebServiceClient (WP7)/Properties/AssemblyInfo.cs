bcfce23d74a9d316239e28451cd217c252727021
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-10-31 16:21:44 -0500

    Added project templates for Windows Phone 7.

--- WP7/UpdateControls.Correspondence.WebServiceClient (WP7)/Properties/AssemblyInfo.cs
+++ WP7/UpdateControls.Correspondence.WebServiceClient (WP7)/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UpdateControls.Correspondence.WebServiceClient__WP7_")]
+[assembly: AssemblyTitle("UpdateControls.Correspondence.WebServiceClient")]
 [assembly: AssemblyDescription("")]
 
 [assembly: Guid("d962b725-6034-440c-86b5-691acd0889cd")]
