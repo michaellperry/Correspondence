3c4b2456faa4d7a175b4fa0f6090064600d56a58
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-01-26 22:47:10 -0600

    Fixed Silverlight POX HTTP requests. Factored out common code.

--- Desktop/UpdateControls.Correspondence.POXClient/POXAsynchronousCommunicationStrategy.cs
+++ Desktop/UpdateControls.Correspondence.POXClient/POXAsynchronousCommunicationStrategy.cs
@@ -10,12 +10,6 @@ namespace UpdateControls.Correspondence.POXClient
 {
     public partial class POXAsynchronousCommunicationStrategy : IAsynchronousCommunicationStrategy
 	{
-        private static XmlSerializerNamespaces DefaultNamespaces = DefineNamespaces();
-		private static XmlSerializer GetRequestSerializer = new XmlSerializer(typeof(GetRequest));
-		private static XmlSerializer GetResponseSerializer = new XmlSerializer(typeof(GetResponse));
-		private static XmlSerializer PostRequestSerializer = new XmlSerializer(typeof(PostRequest));
-		private static XmlSerializer PostResponseSerializer = new XmlSerializer(typeof(PostResponse));
-
         private POXConfiguration _configuration;
 
 		public POXAsynchronousCommunicationStrategy(IPOXConfigurationProvider configurationProvider)
@@ -35,96 +29,38 @@ namespace UpdateControls.Correspondence.POXClient
 
 		public void BeginGet(FactTreeMemento pivotTree, FactID pivotId, TimestampID timestamp, Guid clientGuid, Action<FactTreeMemento, TimestampID> callback)
 		{
-			GetRequest request = new GetRequest
-			{
-				PivotTree = Translate.MementoToFactTree(pivotTree),
-				PivotId = pivotId.key,
-				Timestamp = timestamp.Key,
-				ClientGuid = clientGuid.ToString()
-			};
-			WebRequest webRequest = WebRequest.Create(new Uri(_configuration.Endpoint));
-			webRequest.Method = "POST";
-			webRequest.BeginGetRequestStream(a1 =>
-			{
-				try
-				{
-					Stream requestStream = webRequest.EndGetRequestStream(a1);
-                    GetRequestSerializer.Serialize(requestStream, request, DefaultNamespaces);
-					webRequest.BeginGetResponse(a2 =>
-					{
-						try
-						{
-							WebResponse webResponse = webRequest.EndGetResponse(a2);
-							Stream responseStream = webResponse.GetResponseStream();
-							GetResponse response = (GetResponse)GetResponseSerializer.Deserialize(responseStream);
-							callback(Translate.FactTreeToMemento(response.FactTree), new TimestampID(response.FactTree.DatabaseId, response.Timestamp));
-						}
-						catch (Exception ex)
-						{
-							HandleException(ex);
-                            callback(new FactTreeMemento(pivotTree.DatabaseId), new TimestampID(pivotTree.DatabaseId, timestamp.Key));
-						}
-					}, null);
-				}
-				catch (Exception ex)
-				{
-					HandleException(ex);
-                    callback(new FactTreeMemento(pivotTree.DatabaseId), new TimestampID(pivotTree.DatabaseId, timestamp.Key));
-				}
-			}, null);
-		}
+            GetRequest request = new GetRequest
+            {
+                PivotTree = Translate.MementoToFactTree(pivotTree),
+                PivotId = pivotId.key,
+                Timestamp = timestamp.Key,
+                ClientGuid = clientGuid.ToString()
+            };
+            POXHttpRequest.Begin<GetRequest, GetResponse>(
+                _configuration.Endpoint,
+                request,
+                response => callback(
+                    Translate.FactTreeToMemento(response.FactTree),
+                    new TimestampID(response.FactTree.DatabaseId, response.Timestamp)),
+                ex => callback(
+                    new FactTreeMemento(pivotTree.DatabaseId),
+                    new TimestampID(pivotTree.DatabaseId, timestamp.Key)));
+        }
 
-		public void BeginPost(FactTreeMemento messageBody, Guid clientGuid, Action<bool> callback)
-		{
-			PostRequest request = new PostRequest
-			{
-				MessageBody = Translate.MementoToFactTree(messageBody),
-				ClientGuid = clientGuid.ToString()
-			};
-			WebRequest webRequest = WebRequest.Create(new Uri(_configuration.Endpoint));
-			webRequest.Method = "POST";
-			webRequest.BeginGetRequestStream(a1 =>
-			{
-				try
-				{
-					Stream requestStream = webRequest.EndGetRequestStream(a1);
-                    PostRequestSerializer.Serialize(requestStream, request, DefaultNamespaces);
-					webRequest.BeginGetResponse(a2 =>
-					{
-						try
-						{
-							WebResponse webResponse = webRequest.EndGetResponse(a2);
-							Stream responseStream = webResponse.GetResponseStream();
-							PostResponse response = (PostResponse)PostResponseSerializer.Deserialize(responseStream);
-							callback(true);
-						}
-						catch (Exception ex)
-						{
-							HandleException(ex);
-							callback(false);
-						}
-					}, null);
-				}
-				catch (Exception ex)
-				{
-					HandleException(ex);
-					callback(false);
-				}
-			}, null);
-		}
+        public void BeginPost(FactTreeMemento messageBody, Guid clientGuid, Action<bool> callback)
+        {
+            PostRequest request = new PostRequest
+            {
+                MessageBody = Translate.MementoToFactTree(messageBody),
+                ClientGuid = clientGuid.ToString()
+            };
+            POXHttpRequest.Begin<PostRequest, PostResponse>(
+                _configuration.Endpoint,
+                request,
+                response => callback(true),
+                ex => callback(false));
+        }
 
 		public event Action<FactTreeMemento> MessageReceived;
-
-		private void HandleException(Exception ex)
-		{
-			// TODO
-		}
-
-        private static XmlSerializerNamespaces DefineNamespaces()
-        {
-            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
-            namespaces.Add("c", "http://correspondence.updatecontrols.com/pox/1.0");
-            return namespaces;
-        }
     }
 }
