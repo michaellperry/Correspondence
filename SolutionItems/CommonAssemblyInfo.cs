c98d39ae48742c633f2829b43acbe73703007e8b
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-03-21 19:50:07 -0500

    Added NuGet specs for Windows Phone 7. Version 1.1.0.0.

--- SolutionItems/CommonAssemblyInfo.cs
+++ SolutionItems/CommonAssemblyInfo.cs
@@ -6,12 +6,12 @@ using System;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Update Controls Correspondence")]
-[assembly: AssemblyCopyright("Copyright © Michael L Perry 2010")]
+[assembly: AssemblyCopyright("Copyright © Michael L Perry 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
