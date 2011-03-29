b32cda4600ac801651bee2adb5c11133eac1b3df
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-03-29 07:03:43 -0500

    Updated T4 template in Model package. Added dependency on Silverlight unit test framework for Windows Phone.

--- Desktop/UpdateControls.Correspondence.UnitTest/PropertyTest.cs
+++ Desktop/UpdateControls.Correspondence.UnitTest/PropertyTest.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using UpdateControls.Correspondence.UnitTest.Model;
 using UpdateControls.Correspondence.Memory;
+using UpdateControls.Correspondence.UnitTest.Model;
 
 namespace UpdateControls.Correspondence.UnitTest
 {
---
+++ NuGet.WP7/PackAll.bat
@@ -0,0 +1,4 @@
+nuget pack Core\correspondence.wp7.nuspec
+nuget pack Model\correspondence.model.wp7.nuspec
+nuget pack UnitTest\correspondence.unittest.wp7.nuspec
+nuget pack App\correspondence.app.wp7.nuspec
