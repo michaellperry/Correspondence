b4d20374fec474228832f062d2cd3662ae21affa
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-04-13 23:49:20 -0500

    Fixed remaining NuGet build issues.

---
+++ Silverlight 4/UpdateControls.Correspondence.POXClient/POXAsynchronousCommunicationStrategy.PushNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UpdateControls.Correspondence.Mementos;
+using UpdateControls.Correspondence.Strategy;
+
+namespace UpdateControls.Correspondence.POXClient
+{
+    public class NoOpPushSubscription : IPushSubscription
+    {
+        public void Unsubscribe()
+        {
+        }
+    }
+
+    public partial class POXAsynchronousCommunicationStrategy
+    {
+        public IPushSubscription SubscribeForPush(FactTreeMemento pivotTree, FactID pivotId, Guid clientGuid)
+        {
+            // Push notification is not supported in the silverlight version (yet).
+            return new NoOpPushSubscription();
+        }
+    }
+}
