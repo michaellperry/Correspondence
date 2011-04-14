c9796ae2ecefe0bbbef7a8282c6bd1176a6b5afb
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-04-14 12:14:08 -0500

    Updated references to NuGet package specs.

--- NuGet/PackAll.bat
+++ NuGet/PackAll.bat
@@ -1,5 +1,8 @@
 nuget pack Core\Correspondence.nuspec
 nuget pack Model\Correspondence.Model.nuspec
-nuget pack UnitTest\Correspondence.UnitTest.nuspec
-nuget pack App\Correspondence.App.nuspec
-nuget pack AllInOne\Correspondence.AllInOne.nuspec
+nuget pack WindowsPhone\UnitTest\Correspondence.WindowsPhone.UnitTest.nuspec
+nuget pack WindowsPhone\App\Correspondence.WindowsPhone.App.nuspec
+nuget pack WindowsPhone\AllInOne\Correspondence.WindowsPhone.AllInOne.nuspec
+nuget pack Silverlight\UnitTest\Correspondence.Silverlight.UnitTest.nuspec
+nuget pack Silverlight\App\Correspondence.Silverlight.App.nuspec
+nuget pack Silverlight\AllInOne\Correspondence.Silverlight.AllInOne.nuspec
