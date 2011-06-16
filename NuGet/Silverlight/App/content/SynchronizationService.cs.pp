196ad2fda0b83255198754c8a1951e2318a53535
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-06-16 15:52:42 -0500

    Added System.Windows.Threading to SynchronizationService template.

--- NuGet/Silverlight/App/content/SynchronizationService.cs.pp
+++ NuGet/Silverlight/App/content/SynchronizationService.cs.pp
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Threading;
 using UpdateControls.Correspondence;
 using UpdateControls.Correspondence.IsolatedStorage;
 using UpdateControls.Correspondence.POXClient;
