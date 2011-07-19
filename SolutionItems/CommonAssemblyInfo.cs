1dc0194c948c45b97b9efabcfc7bcadf2ece8f05
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-07-19 17:48:35 -0500

    Updated version number in T4 template.

--- SolutionItems/CommonAssemblyInfo.cs
+++ SolutionItems/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.1")]
