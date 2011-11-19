806e53196c04f7e20b81ad49d542835c2a8fd09d
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-11-19 17:42:36 -0600

    Added binary HTTP projects for Silveright 4 and WP 7

---
+++ Silverlight4/UpdateControls.Correspondence.BinaryHTTPClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls.Correspondence.BinaryHTTPClient")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("7bba576a-1b9d-46c3-956f-f7a6fe226597")]
---
+++ WP7/UpdateControls.Correspondence.BinaryHTTPClient/BinaryHTTPAsynchronousCommunicationStrategy.PushNotification.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Phone.Notification;
+using UpdateControls.Correspondence.Mementos;
+using UpdateControls.Correspondence.Strategy;
+using System.IO;
+
+namespace UpdateControls.Correspondence.BinaryHTTPClient
+{
+    public partial class BinaryHTTPAsynchronousCommunicationStrategy
+    {
+        private HttpNotificationChannel _httpChannel;
+        private Dictionary<FactID, WindowsPhonePushSubscription> _subscriptionByFactId = new Dictionary<FactID, WindowsPhonePushSubscription>();
+        private bool _receivedChannelUri = false;
+        private bool _openPending = false;
+
+        private ToastNotificationObserver _toastNotificationObserver;
+
+        private object _monitor = new object();
+
+        partial void Initialize()
+        {
+            _toastNotificationObserver = new ToastNotificationObserver(_configurationProvider);
+        }
+
+        public bool IsLongPolling
+        {
+            get { return false; }
+        }
+
+        public IPushSubscription SubscribeForPush(FactTreeMemento pivotTree, FactID pivotId, Guid clientGuid)
+        {
+            lock (_monitor)
+            {
+                WindowsPhonePushSubscription subscription;
+                if (!_subscriptionByFactId.TryGetValue(pivotId, out subscription))
+                {
+                    subscription = new WindowsPhonePushSubscription(_configuration, pivotTree, pivotId.key, clientGuid, _monitor, UpdateSubscriptions);
+                    _subscriptionByFactId.Add(pivotId, subscription);
+                }
+                subscription.ShouldBeSubscribed = true;
+                UpdateSubscriptions();
+                return subscription;
+            }
+        }
+
+        private void UpdateSubscriptions()
+        {
+            if (_httpChannel == null)
+            {
+                //First, try to pick up existing channel
+                _httpChannel = HttpNotificationChannel.Find(_configuration.ChannelName);
+                if (_httpChannel != null)
+                {
+                    _receivedChannelUri = _httpChannel.ChannelUri != null;
+                    _openPending = !_receivedChannelUri;
+                    SubscribeToChannelEvents();
+                    _toastNotificationObserver.HttpChannel = _httpChannel;
+                }
+                else
+                {
+                    //Create the channel
+                    _httpChannel = new HttpNotificationChannel(_configuration.ChannelName, "UpdateControls.Correspondence");
+                    SubscribeToChannelEvents();
+                }
+            }
+
+            if (!_receivedChannelUri)
+            {
+                if (!_openPending)
+                {
+                    _openPending = true;
+                    _httpChannel.Open();
+                    _toastNotificationObserver.HttpChannel = _httpChannel;
+                }
+            }
+            else
+            {
+                string deviceUri = _httpChannel.ChannelUri.ToString();
+                foreach (WindowsPhonePushSubscription subscription in _subscriptionByFactId.Values)
+                {
+                    subscription.UpdateSubscription(deviceUri);
+                }
+            }
+        }
+
+        private void SubscribeToChannelEvents()
+        {
+            _httpChannel.ChannelUriUpdated += httpChannel_ChannelUriUpdated;
+            _httpChannel.HttpNotificationReceived += httpChannel_HttpNotificationReceived;
+            _httpChannel.ErrorOccurred += httpChannel_ExceptionOccurred;
+        }
+
+        private void httpChannel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+        {
+            lock (_monitor)
+            {
+                _receivedChannelUri = true;
+                _openPending = false;
+                UpdateSubscriptions();
+            }
+        }
+
+        private void httpChannel_ExceptionOccurred(object sender, NotificationChannelErrorEventArgs e)
+        {
+            lock (_monitor)
+            {
+                _openPending = false;
+            }
+        }
+
+        private void httpChannel_HttpNotificationReceived(object sender, HttpNotificationEventArgs e)
+        {
+            if (MessageReceived != null)
+            {
+                using (BinaryReader factReader = new BinaryReader(e.Notification.Body))
+                {
+                    FactTreeMemento factTreeMemento = ReadFactTreeFromStorage(factReader);
+                    MessageReceived(factTreeMemento);
+                }
+            }
+        }
+
+        private static FactTreeMemento ReadFactTreeFromStorage(BinaryReader factReader)
+        {
+            FactTreeMemento factTreeMemento = new FactTreeMemento(0);
+            int factCount = factReader.ReadInt32();
+            for (int i = 0; i < factCount; i++)
+            {
+                factTreeMemento.Add(ReadFactFromStorage(factReader));
+            }
+            return factTreeMemento;
+        }
+
+        private static IdentifiedFactMemento ReadFactFromStorage(BinaryReader factReader)
+        {
+            long factId;
+            string typeName;
+            int version;
+            short dataSize;
+            byte[] data;
+            short predecessorCount;
+
+            factId = factReader.ReadInt64();
+            typeName = factReader.ReadString();
+            version = factReader.ReadInt32();
+            dataSize = factReader.ReadInt16();
+            data = dataSize > 0 ? factReader.ReadBytes(dataSize) : new byte[0];
+            predecessorCount = factReader.ReadInt16();
+
+            FactMemento factMemento = new FactMemento(new CorrespondenceFactType(typeName, version));
+            factMemento.Data = data;
+            for (short i = 0; i < predecessorCount; i++)
+            {
+                string declaringTypeName;
+                int declaringTypeVersion;
+                string roleName;
+                bool isPivot;
+                long predecessorFactId;
+
+                declaringTypeName = factReader.ReadString();
+                declaringTypeVersion = factReader.ReadInt32();
+                roleName = factReader.ReadString();
+                isPivot = factReader.ReadBoolean();
+                predecessorFactId = factReader.ReadInt64();
+
+                factMemento.AddPredecessor(
+                    new RoleMemento(
+                        new CorrespondenceFactType(declaringTypeName, declaringTypeVersion),
+                        roleName,
+                        null,
+                        isPivot),
+                    new FactID() { key = predecessorFactId },
+                    isPivot
+                );
+            }
+
+            return new IdentifiedFactMemento(new FactID { key = factId }, factMemento);
+        }
+    }
+}
