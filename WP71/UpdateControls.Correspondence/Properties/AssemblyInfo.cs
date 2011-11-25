184cfbcff668d6bb58b401c746ba01b44d0af9dd
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-11-25 09:58:25 -0600

    Upgraded UpdateControls. Added WP71 projects.

---
+++ WP71/UpdateControls.Correspondence/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls.Correspondence")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("d4516c59-d617-42ed-8ee5-a7b7f9c1ca5a")]
