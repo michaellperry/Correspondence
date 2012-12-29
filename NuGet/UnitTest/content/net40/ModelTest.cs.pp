6d4db3cc6ea44dc77792f839129a11a84dd3ed1c
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2012-12-29 14:03:04 -0600

    Consolidate all unit test packages into one.

---
+++ NuGet/UnitTest/content/net40/ModelTest.cs.pp
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UpdateControls.Correspondence;
+using UpdateControls.Correspondence.Memory;
+
+namespace $rootnamespace$
+{
+    [TestClass]
+    public class ModelTest
+    {
+        private Community _communityFlynn;
+        private Community _communityAlan;
+        private Individual _individualFlynn;
+        private Individual _individualAlan;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var sharedCommunication = new MemoryCommunicationStrategy();
+            _communityFlynn = new Community(new MemoryStorageStrategy())
+                .AddCommunicationStrategy(sharedCommunication)
+                .Register<CorrespondenceModel>()
+                .Subscribe(() => _individualFlynn)
+				;
+            _communityAlan = new Community(new MemoryStorageStrategy())
+                .AddCommunicationStrategy(sharedCommunication)
+                .Register<CorrespondenceModel>()
+                .Subscribe(() => _individualAlan)
+                ;
+
+            _individualFlynn = _communityFlynn.AddFact(new Individual("flynn"));
+            _individualAlan = _communityAlan.AddFact(new Individual("alan"));
+        }
+
+        private void Synchronize()
+        {
+            while (_communityFlynn.Synchronize() || _communityAlan.Synchronize()) ;
+        }
+	}
+}
