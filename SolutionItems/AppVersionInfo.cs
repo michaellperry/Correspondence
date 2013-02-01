6633f7618f5aa2952b04d29abd314bfea4a3c282
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2013-02-01 12:16:36 -0600

    Version 1.4.2.0.

--- SolutionItems/AppVersionInfo.cs
+++ SolutionItems/AppVersionInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-[assembly: AssemblyVersion("1.4.1.3")]
-[assembly: AssemblyFileVersion("1.4.1.3")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.2.0")]
