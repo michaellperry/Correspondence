a4caae7ed93cef7a232b4f53d6f72f87b4c79585
Michael L Perry <michael@qedcode.com>
2013-12-24 11:54:40 -0600

    Silverlight Binary HTTP client.

---
+++ Silverlight/UpdateControls.Correspondence.BinaryHTTPClient/BinaryHTTPAsynchronousCommunicationStrategy.PushNotification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UpdateControls.Correspondence.Mementos;
+using UpdateControls.Correspondence.Strategy;
+
+namespace UpdateControls.Correspondence.BinaryHTTPClient
+{
+    public class NoOpPushSubscription : IPushSubscription
+    {
+        public void Unsubscribe()
+        {
+        }
+    }
+
+    public partial class BinaryHTTPAsynchronousCommunicationStrategy
+    {
+        public IPushSubscription SubscribeForPush(FactTreeMemento pivotTree, FactID pivotId, Guid clientGuid)
+        {
+            // Push notification is not supported in the desktop version.
+            return new NoOpPushSubscription();
+        }
+
+        public bool IsLongPolling
+        {
+            get { return _configuration.TimeoutSeconds > 0; }
+        }
+    }
+}
---
+++ Silverlight/UpdateControls.Correspondence.BinaryHTTPClient/BinaryHTTPAsynchronousCommunicationStrategy.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UpdateControls.Correspondence.Mementos;
+using UpdateControls.Correspondence.Strategy;
+
+namespace UpdateControls.Correspondence.BinaryHTTPClient
+{
+    public partial class BinaryHTTPAsynchronousCommunicationStrategy : IAsynchronousCommunicationStrategy
+    {
+        private const string DefaultAPIKey = "<<Your API key>>";
+
+        private HTTPConfiguration _configuration;
+        private IHTTPConfigurationProvider _configurationProvider;
+
+        public BinaryHTTPAsynchronousCommunicationStrategy(IHTTPConfigurationProvider configurationProvider)
+		{
+            _configurationProvider = configurationProvider;
+			_configuration = configurationProvider.Configuration;
+
+            Initialize();
+		}
+
+        partial void Initialize();
+
+        public string ProtocolName
+        {
+            get { return "http://correspondence.updatecontrols.net/bin"; }
+        }
+
+        public string PeerName
+        {
+            get { return _configuration.Endpoint; }
+        }
+
+        public void BeginGetMany(FactTreeMemento pivotTree, List<PivotMemento> pivots, Guid clientGuid, Action<FactTreeMemento, IEnumerable<PivotMemento>> callback, Action<Exception> error)
+        {
+            if (_configuration.APIKey == DefaultAPIKey)
+            {
+                error(new ArgumentException("Please register for an API key at http://CorrespondenceCloud.com"));
+                return;
+            }
+
+            GetManyRequest request = new GetManyRequest
+            {
+                Domain = _configuration.APIKey,
+                PivotTree = pivotTree,
+                PivotIds = pivots
+                    .Select(pivot => new FactTimestamp { FactId = pivot.PivotId.key, TimestampId = pivot.Timestamp.Key })
+                    .ToList(),
+                ClientGuid = clientGuid.ToString(),
+                TimeoutSeconds = _configuration.TimeoutSeconds
+            };
+            BinaryHTTPRequest.Begin(
+                _configuration,
+                request,
+                response => callback(
+                    ((GetManyResponse)response).FactTree,
+                    ((GetManyResponse)response).PivotIds
+                        .Select(pivot => new PivotMemento(
+                            new FactID { key = pivot.FactId },
+                            new TimestampID(0L, pivot.TimestampId)))),
+                ex =>
+                {
+                    error(ex);
+                });
+        }
+
+        public void BeginPost(FactTreeMemento messageBody, Guid clientGuid, List<UnpublishMemento> unpublishedMessages, Action<bool> callback, Action<Exception> error)
+        {
+            if (_configuration.APIKey == DefaultAPIKey)
+            {
+                callback(false);
+                error(new ArgumentException("Please register for an API key at http://qedcode.com/correspondence"));
+                return;
+            }
+
+            PostRequest request = new PostRequest
+            {
+                Domain = _configuration.APIKey,
+                MessageBody = messageBody,
+                ClientGuid = clientGuid.ToString(),
+                UnpublishedMessages = unpublishedMessages
+            };
+            BinaryHTTPRequest.Begin(
+                _configuration,
+                request,
+                response => callback(true),
+                ex =>
+                {
+                    callback(false);
+                    error(ex);
+                });
+        }
+
+        public void Interrupt(Guid clientGuid)
+        {
+            if (_configuration.APIKey == DefaultAPIKey)
+                return;
+
+            InterruptRequest request = new InterruptRequest
+            {
+                Domain = _configuration.APIKey,
+                ClientGuid = clientGuid.ToString()
+            };
+            BinaryHTTPRequest.Begin(
+                _configuration,
+                request,
+                response => { },
+                ex => { });
+        }
+
+        public void Notify(FactTreeMemento messageBody, FactID pivotId, Guid clientGuid, string text1, string text2)
+        {
+            if (_configuration.APIKey == DefaultAPIKey)
+                return;
+
+            NotifyRequest request = new NotifyRequest
+            {
+                Domain = _configuration.APIKey,
+                PivotTree = messageBody,
+                PivotId = pivotId.key,
+                ClientGuid = clientGuid.ToString(),
+                Text1 = text1,
+                Text2 = text2
+            };
+            BinaryHTTPRequest.Begin(
+                _configuration,
+                request,
+                response => { },
+                ex => { });
+        }
+
+        public event Action<FactTreeMemento> MessageReceived;
+    }
+}
