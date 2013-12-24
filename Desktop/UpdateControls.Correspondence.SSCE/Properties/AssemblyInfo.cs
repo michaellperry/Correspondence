343db5ac1c2357b182feb04d7e72b271bbc2b470
Michael L Perry <michael@qedcode.com>
2013-12-24 13:13:04 -0600

    SSCE for WPF.

--- Desktop/UpdateControls.Correspondence.SSCE/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UpdateControls.Correspondence.SSCE")]
-[assembly: AssemblyDescription("")]
-
-[assembly: Guid("8be69176-9548-4e3e-b6da-48a63e1a2734")]
---
+++ WPF/UpdateControls.Correspondence.SSCE/ConditionWriter.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UpdateControls.Correspondence.Queries;
+
+namespace UpdateControls.Correspondence.SSCE
+{
+    internal class ConditionWriter
+    {
+        private SSCEStorageStrategy _storageStrategy;
+        private Session _session;
+        private StringBuilder _conditions;
+        private List<int> _roleIds;
+        private int _depth;
+        private string _branchAlias;
+
+        public ConditionWriter(SSCEStorageStrategy storageStrategy, Session session, StringBuilder conditions, List<int> roleIds, int depth, string branchAlias)
+        {
+            _storageStrategy = storageStrategy;
+            _session = session;
+            _conditions = conditions;
+            _roleIds = roleIds;
+            _depth = depth;
+            _branchAlias = branchAlias;
+        }
+
+        public void VisitSimple(bool isEmpty, QueryDefinition subQuery)
+        {
+            _conditions.Append("\r\nAND ");
+            if (isEmpty)
+                _conditions.Append("NOT ");
+            StringBuilder subConditions = new StringBuilder();
+            List<Join> joins = subQuery.Joins.Reverse().ToList();
+            Join first = joins[0];
+            joins.RemoveAt(0);
+
+            int index = _roleIds.Count;
+            _roleIds.Add(_storageStrategy.SaveRole(_session, first.Role));
+            string nextAlias = "p" + index;
+
+            string priorAlias;
+            string whereAlias;
+            if (first.Successor)
+            {
+                priorAlias = nextAlias + ".FKPredecessorFactID";
+                whereAlias = nextAlias + ".FKFactID";
+            }
+            else
+            {
+                priorAlias = nextAlias + ".FKFactID";
+                whereAlias = nextAlias + ".FKPredecessorFactID";
+            }
+
+            if (first.Condition != null)
+            {
+                ConditionWriter writer = new ConditionWriter(_storageStrategy, _session, subConditions, _roleIds, _depth + 1, whereAlias);
+                foreach (var clause in first.Condition.Clauses)
+                    writer.VisitSimple(clause.IsEmpty, clause.SubQuery);
+            }
+
+            _conditions.AppendFormat("EXISTS (SELECT 1 FROM Predecessor {0} ", nextAlias);
+
+            string factAlias = _storageStrategy.AppendJoins(_session, joins, _conditions, _roleIds, subConditions, _depth + 1, priorAlias);
+            _conditions.AppendFormat("\r\nWHERE {0}={1} AND {2}.FKRoleID=@Role{3} ", factAlias, _branchAlias, nextAlias, index);
+            _conditions.Append(subConditions);
+            _conditions.Append(") ");
+        }
+    }
+}
