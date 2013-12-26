7fc5edae717f8a1ca2f4dba3c05181eb1382da66
Michael L Perry <michael@qedcode.com>
2013-12-26 16:46:25 -0600

    Updated NuGet packages.

--- NuGet/PackAll.bat
+++ NuGet/PackAll.bat
@@ -1,7 +1,10 @@
-nuget pack Core\Correspondence.nuspec
-nuget pack Model\Correspondence.Model.nuspec
-nuget pack UnitTest\Correspondence.UnitTest.nuspec
-nuget pack App\Correspondence.App.nuspec
-nuget pack AllInOne\Correspondence.AllInOne.nuspec
-nuget pack Web\App\Correspondence.Web.App.nuspec
-nuget pack Web\AllInOne\Correspondence.Web.AllInOne.nuspec
+..\.nuget\nuget pack Core\Correspondence.nuspec
+..\.nuget\nuget pack Model\Correspondence.Model.nuspec
+..\.nuget\nuget pack UnitTest\Correspondence.UnitTest.nuspec
+..\.nuget\nuget pack UnitTest.Core\Correspondence.UnitTest.Core.nuspec
+..\.nuget\nuget pack App\Correspondence.App.nuspec
+..\.nuget\nuget pack App.Core\Correspondence.App.Core.nuspec
+..\.nuget\nuget pack AllInOne\Correspondence.AllInOne.nuspec
+..\.nuget\nuget pack Web.App\Correspondence.Web.App.nuspec
+..\.nuget\nuget pack Web.App.Core\Correspondence.Web.App.Core.nuspec
+..\.nuget\nuget pack Web.AllInOne\Correspondence.Web.AllInOne.nuspec
--- SolutionItems/CoreVersionInfo.cs
+++ SolutionItems/CoreVersionInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
