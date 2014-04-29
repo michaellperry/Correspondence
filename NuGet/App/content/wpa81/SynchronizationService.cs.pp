37713d4d109e9e1785c4399f6f6acea0b9cf24c3
Michael L Perry <michael@qedcode.com>
2014-04-28 21:27:28 -0500

    Copy to Windows Phone 8.1 folder.

---
+++ NuGet/App/content/wpa81/SynchronizationService.cs.pp
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using UpdateControls.Correspondence;
+using UpdateControls.Correspondence.FileStream;
+using UpdateControls.Correspondence.BinaryHTTPClient;
+using UpdateControls.Correspondence.BinaryHTTPClient.Notification;
+using UpdateControls.Fields;
+using UpdateControls.Correspondence.Memory;
+
+namespace $rootnamespace$
+{
+    public class SynchronizationService
+    {
+        private const string ThisIndividual = "$rootnamespace$.Individual.this";
+        private static readonly Regex Punctuation = new Regex(@"[{}\-]");
+
+        private Community _community;
+        private Independent<Individual> _individual = new Independent<Individual>(
+            Individual.GetNullInstance());
+
+        public void Initialize()
+        {
+            var storage = new FileStreamStorageStrategy();
+            var http = new HTTPConfigurationProvider();
+            var communication = new BinaryHTTPAsynchronousCommunicationStrategy(http);
+            var notification = new WindowsPhoneNotificationStrategy(http);
+            communication.SetNotificationStrategy(notification);
+
+            _community = new Community(storage);
+            _community.AddAsynchronousCommunicationStrategy(communication);
+            _community.Register<CorrespondenceModel>();
+            _community.Subscribe(() => Individual);
+
+            CreateIndividual(http);
+
+            // Synchronize whenever the user has something to send.
+            _community.FactAdded += delegate
+            {
+                Synchronize();
+            };
+
+            // Synchronize when the network becomes available.
+            System.Net.NetworkInformation.NetworkChange.NetworkAddressChanged += (sender, e) =>
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                    Synchronize();
+            };
+
+            // And synchronize on startup or resume.
+            Synchronize();
+        }
+
+        public void InitializeDesignData()
+        {
+            _community = new Community(new MemoryStorageStrategy());
+            _community.Register<CorrespondenceModel>();
+
+            CreateIndividualDesignData();
+        }
+
+        public Community Community
+        {
+            get { return _community; }
+        }
+
+        public Individual Individual
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _individual;
+                }
+            }
+            private set
+            {
+                lock (this)
+                {
+                    _individual.Value = value;
+                }
+            }
+        }
+
+        public void Synchronize()
+        {
+            _community.BeginSending();
+            _community.BeginReceiving();
+        }
+
+        private void CreateIndividual(HTTPConfigurationProvider http)
+        {
+			_community.Perform(async delegate
+			{
+				Individual individual = await _community.LoadFactAsync<Individual>(ThisIndividual);
+				if (individual == null)
+				{
+					string randomId = Punctuation.Replace(Guid.NewGuid().ToString(), String.Empty).ToLower();
+					individual = await _community.AddFactAsync(new Individual(randomId));
+					await _community.SetFactAsync(ThisIndividual, individual);
+				}
+				Individual = individual;
+				http.Individual = individual;
+			});
+        }
+
+        private void CreateIndividualDesignData()
+        {
+			_community.Perform(async delegate
+			{
+				var individual = await _community.AddFactAsync(new Individual("design"));
+				Individual = individual;
+			});
+        }
+    }
+}
