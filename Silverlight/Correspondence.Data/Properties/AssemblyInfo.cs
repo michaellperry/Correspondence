1ee26ef15ccea73d2ce76f40df99010e6f079261
Michael L Perry <michael@qedcode.com>
2015-07-19 12:08:19 -0500

    Assemble files in nuspec rather than post build step.

--- Silverlight/Correspondence.Data/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UpdateControls.Correspondence.Data")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("692021bb-b417-4480-b525-a9dc102b82f9")]
