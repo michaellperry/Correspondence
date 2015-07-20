d85b061e8d483301113221e9a290f06603f120da
Michael L Perry <michael@qedcode.com>
2015-07-20 17:46:31 -0500

    Version 3.0.0.5.

--- SolutionItems/CoreVersionInfo.cs
+++ SolutionItems/CoreVersionInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.10")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
